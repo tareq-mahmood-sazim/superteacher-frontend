f914d6fea21a0508131d2336f1f966268dceabb6
tareq-mahmood-1-sazim <tareq.mahmood@sazim.io>
2024-10-27 00:03:44 +0600

    feat(ST-41): adding and getting participants

---
+++ modules/classroom/components/addStudentOnClassroom/AddStudentOnClassroom.tsx
@@ -0,0 +1,20 @@
+import { Modal } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { CiSquarePlus } from "react-icons/ci";
+
+import AddStudentOnClassroomForm from "@/modules/classroom/components/addStudentOnClassroomForm";
+
+export default function AddStudentOnClassroom() {
+  const [opened, { open, close }] = useDisclosure(false);
+
+  return (
+    <>
+      <Modal opened={opened} onClose={close} centered>
+        <AddStudentOnClassroomForm />
+      </Modal>
+      <button onClick={open} className="text-green-500 rounded-md">
+        <CiSquarePlus className="text-4xl" />
+      </button>
+    </>
+  );
+}
