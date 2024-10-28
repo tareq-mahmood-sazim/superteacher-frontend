5c7e45bda86a58a7e308cceddd9bc92bda98ab9f
tareq-mahmood <tareq.mahmood@sazim.io>
2024-10-28 17:30:50 +0600

    refactor: added interface for createClassModal

--- shared/components/Navbar/components/createClassroom/createClassroomModal.tsx
+++ shared/components/Navbar/components/createClassroom/createClassroomModal.tsx
@@ -1,9 +1,13 @@
 import { Modal, Button } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 
-import CreateClassForm from "./components/createClassroomForm";
+import CreateClassForm from "./components/createClassForm";
 
-export default function CreateClassModal({ buttonName }: { buttonName?: string }) {
+interface ICreateClassModalProps {
+  buttonLabel?: string;
+}
+
+export default function CreateClassModal({ buttonLabel }: ICreateClassModalProps) {
   const [opened, { open, close }] = useDisclosure(false);
 
   return (
@@ -14,10 +18,10 @@ export default function CreateClassModal({ buttonName }: { buttonName?: string }
 
       <Button
         variant="none"
-        className="text-center text-white border-white border-1 bg-black"
+        classLabel="text-center text-white border-white border-1 bg-black"
         onClick={open}
       >
-        {buttonName ? buttonName : "Create Class"}
+        {buttonLabel ? buttonLabel : "Create Class"}
       </Button>
     </>
   );
