82ea2aedcfe031fa316aea7e8592131f11608054
tareq-mahmood <tareq.mahmood@sazim.io>
2024-11-04 17:52:10 +0600

    refactor: modified classroom tab container with constant value

--- modules/classroom/containers/classroomContainer/ClassroomContainer.tsx
+++ modules/classroom/containers/classroomContainer/ClassroomContainer.tsx
@@ -1,28 +1,31 @@
 import { Tabs } from "@mantine/core";
-
 import PeopleContainer from "@/modules/classroom/containers/peopleContainer";
 
 export default function ClassroomContainer() {
+  const TAB_ITEMS = {
+    STREAM: "stream",
+    CLASSWORK: "classwork",
+    PEOPLE: "people",
+  };
+
   return (
     <div className="mx-8">
-      <Tabs defaultValue="stream">
+      <Tabs defaultValue={TAB_ITEMS.STREAM}>
         <Tabs.List grow className="text-white justify-center">
-          <Tabs.Tab value="stream" color="teal" className="hover:bg-transparent">
+          <Tabs.Tab value={TAB_ITEMS.STREAM} color="teal" className="hover:bg-transparent">
             <span className="text-white">Stream</span>
           </Tabs.Tab>
-          <Tabs.Tab value="classwork" color="teal" className="hover:bg-transparent">
+          <Tabs.Tab value={TAB_ITEMS.CLASSWORK} color="teal" className="hover:bg-transparent">
             <span className="text-white">Classroom</span>
           </Tabs.Tab>
-          <Tabs.Tab value="people" color="teal" className="hover:bg-transparent">
+          <Tabs.Tab value={TAB_ITEMS.PEOPLE} color="teal" className="hover:bg-transparent">
             <span className="text-white">People</span>
           </Tabs.Tab>
         </Tabs.List>
 
-        <Tabs.Panel value="stream">stream tab content</Tabs.Panel>
-
-        <Tabs.Panel value="classwork">classwork tab content</Tabs.Panel>
-
-        <Tabs.Panel value="people">
+        <Tabs.Panel value={TAB_ITEMS.STREAM}>stream tab content</Tabs.Panel>
+        <Tabs.Panel value={TAB_ITEMS.CLASSWORK}>classwork tab content</Tabs.Panel>
+        <Tabs.Panel value={TAB_ITEMS.PEOPLE}>
           <PeopleContainer />
         </Tabs.Panel>
       </Tabs>
