8f07f9034442228e487d1735b947a0855a4fd090
tareq-mahmood <tareq.mahmood@sazim.io>
2024-11-08 17:22:24 +0600

    feat(ST-41): adding and getting participants (#9)
    * feat(ST-41): adding and getting participants
    
    * fix: build fixed by removing unused component, refactored component names

--- shared/redux/rtk-apis/users/users.types.ts
+++ shared/redux/rtk-apis/users/users.types.ts
@@ -4,4 +4,16 @@ export type TUserProfile = {
   updatedAt: string;
   firstName: string;
   lastName: string;
+  gender?: string;
+  role?: number;
+  highestEducationLevel?: number | null;
+  majorSubject?: string | null;
+  subjectsToTeach?: string | null;
+  educationLevel?: number;
+  medium?: string | null;
+  classLevel?: string | null;
+  degree?: string | null;
+  major?: string | null;
+  semesterOrYear?: string | null;
+  user?: number;
 };
