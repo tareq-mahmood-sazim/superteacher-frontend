cab412702052b7ef284d549b4bf019227a39da9c
tareq-mahmood-1-sazim <tareq.mahmood@sazim.io>
2024-11-18 14:26:48 +0600

    refactor(ST-60): build fixed

--- modules/classroom/components/classroom/createSubmission/hooks/CreateSubmission.hooks.tsx
+++ modules/classroom/components/classroom/createSubmission/hooks/CreateSubmission.hooks.tsx
@@ -1,19 +1,12 @@
 import { useForm, zodResolver } from "@mantine/form";
-import { showNotification } from "@mantine/notifications";
 import { useSelector } from "react-redux";
 
-import { useFileProcessingHook } from "@/shared/hooks/useFileProcessingHook";
-import { useCreateSubmissionMutation } from "@/shared/redux/rtk-apis/submissions/submission.api";
 import { TRootState } from "@/shared/redux/store";
-import { NotificationMessage } from "@/shared/utils/notificationMessage";
 
 import { submissionSchema } from "../helpers/submission.validation";
 import type { SubmissionFormData } from "../helpers/submission.validation";
 
 export default function CreateSubmissionHook(materialId: number, closeButton: () => void) {
-  const [createSubmission, { isLoading: isSubmitting, isError: submissionFailed }] =
-    useCreateSubmissionMutation();
-  const { FileProcessing } = useFileProcessingHook();
   const userId = useSelector((state: TRootState) => state.authenticatedUser.userId);
   const form = useForm<SubmissionFormData>({
     initialValues: {
@@ -24,24 +17,17 @@ export default function CreateSubmissionHook(materialId: number, closeButton: ()
     validate: zodResolver(submissionSchema),
   });
 
-  const onSubmit = async (data: SubmissionFormData) => {
+  const onSubmit = (data: SubmissionFormData) => {
     try {
-      data.attachment = await FileProcessing(data.attachment);
-      const response = await createSubmission(data);
-      if (response) {
-        showNotification(NotificationMessage("Success", "Submitted"));
-        closeButton();
-      }
-      if (submissionFailed) {
-        showNotification(NotificationMessage("Error", "Failed to submit"));
-      }
+      console.log(data);
+      closeButton();
     } catch (err) {
       console.error("Error creating submission:", err);
     }
   };
   return {
     form,
-    isSubmitting,
     onSubmit,
+    isSubmitting: true,
   };
 }
