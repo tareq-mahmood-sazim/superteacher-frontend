605de9bf4c4d097077588f5807a5cc21639d5d67
tareq-mahmood <tareq.mahmood@sazim.io>
2024-11-19 08:40:53 +0000

    refactor(ST-60): removed console statements, added types and separated nested components

--- modules/classroom/components/classroom/createMaterials/addMaterials/AddMaterials.tsx
+++ modules/classroom/components/classroom/createMaterials/addMaterials/AddMaterials.tsx
@@ -5,21 +5,15 @@ import { Button, Modal, TextInput, Textarea, FileInput } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { useForm, Controller } from "react-hook-form";
 
+import type { MaterialFormValues } from "../CreateMaterials.types";
 import { MaterialSchema } from "./helpers/material.validation";
 
 const LuFileEdit = dynamic(() => import("react-icons/lu").then((mod) => mod.LuFileEdit));
 
-type FormValues = {
-  title: string;
-  instructions: string;
-  dueDate: Date | null;
-  attachments?: File[];
-  classroomId: number;
-};
 
 export default function AddMaterials() {
   const [opened, { open, close }] = useDisclosure(false);
-  const { control, handleSubmit } = useForm<FormValues>({
+  const { control, handleSubmit } = useForm<MaterialFormValues>({
     resolver: zodResolver(MaterialSchema),
     defaultValues: {
       title: "",
@@ -29,9 +23,10 @@ export default function AddMaterials() {
     },
   });
 
-  const onSubmit = (data: FormValues) => {
-    console.log(data);
-  };
+  const onSubmit = (data: MaterialFormValues) =>
+    // todo -> implement submission logic here
+     data
+  ;
 
   return (
     <>
